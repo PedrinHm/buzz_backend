3030648c67970038d15e46a88627902339ec1ad4
PedrinHm <pedrohm441@gmail.com>
2024-10-09 17:07:47 -0300

    teste

--- app/dependencies/notifications.py
+++ app/dependencies/notifications.py
@@ -4,11 +4,23 @@ import os
 import json
 from fastapi import HTTPException
 
+# Carregar as credenciais do Firebase a partir da variável de ambiente
 firebase_credentials = os.getenv('FIREBASE_CREDENTIALS_JSON')
-cred_dict = json.loads(firebase_credentials)
-cred = credentials.Certificate(cred_dict)
 
-firebase_admin.initialize_app(cred)
+# Verificar se as credenciais foram fornecidas
+if not firebase_credentials:
+    raise ValueError("As credenciais do Firebase não foram fornecidas ou estão vazias. Certifique-se de definir a variável de ambiente 'FIREBASE_CREDENTIALS_JSON'.")
+
+try:
+    # Carregar as credenciais em formato JSON
+    cred_dict = json.loads(firebase_credentials)
+    cred = credentials.Certificate(cred_dict)
+except json.JSONDecodeError as e:
+    raise ValueError(f"Erro ao decodificar as credenciais do Firebase: {str(e)}")
+
+# Inicializar o aplicativo Firebase somente se ainda não estiver inicializado
+if not firebase_admin._apps:
+    firebase_admin.initialize_app(cred)
 
 async def send_push_notification(token: str, title: str, body: str):
     # Cria a mensagem de notificação
